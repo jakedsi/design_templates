834ad69079e786cbea7eb272cc7baccb91516727
Jake Aboyot <116138724+jakedsi@users.noreply.github.com>
2022-11-27 11:24:28 +0800

    glassmorphism

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Reactslider from './component/Reactslider'
-
+import Glassmorphism from './component/Glassmorphism'
 export default function App() {
   return (
     <div>
       <Reactslider />
+      <Glassmorphism />
     </div>
   )
 }
